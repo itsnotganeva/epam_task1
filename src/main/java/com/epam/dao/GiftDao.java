91c40990519b5ce1a12e8188c3c33fcbf7d1b561
itsnotganeva <ganevich.matvei@gmail.com>
2021-10-09 16:04:49 +0300

    Added loggers

--- src/main/java/com/epam/dao/GiftDao.java
+++ src/main/java/com/epam/dao/GiftDao.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -13,9 +15,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class GiftDaoImpl implements GiftDao {
+public class GiftDao implements IGiftDao {
 
-    private final String FILE_PATH = "D:\\JavaProjects\\EPAM\\epam_task1\\presents.json";
+    private final String FILE_PATH = "D:\\JavaProjects\\EPAM\\epam_task1\\gift.json";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GiftDao.class);
+
 
     @Override
     public void writeGift(Gift gift) {
@@ -47,28 +52,39 @@ public class GiftDaoImpl implements GiftDao {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        LOGGER.info("The sweets of gift: {}", gift.getSweetsList());
         return gift;
     }
 
+    @Override
     public double getWeightOfGift(Gift gift) {
         double weight = 0;
         for (Sweets sweets : gift.getSweetsList()) {
             weight+=sweets.getWeight();
         }
+        LOGGER.info("The weigth of gift is {}", weight);
         return weight;
     }
 
+    @Override
     public List<Sweets> sortSweetsByCalorie(Gift gift) {
         Collections.sort(gift.getSweetsList(), ((o1, o2) -> o1.getCalorieContent() - o2.getCalorieContent()));
+        LOGGER.info("The sorted list of sweets in gift: {}",gift.getSweetsList());
         return gift.getSweetsList();
     }
 
-    public List<Sweets> findSweetsBySugarContent(int startPosition, int finalPosition, Gift gift) {
+    @Override
+    public List<Sweets> getSweetsBySugarContent(int startPosition, int finalPosition, Gift gift) {
         List<Sweets> foundSweets = new ArrayList<>();
         for (Sweets sweets : gift.getSweetsList()) {
             if (sweets.getSugarContent() >= startPosition && sweets.getSugarContent() <= finalPosition) {
                 foundSweets.add(sweets);
+                LOGGER.debug("Sweet with sugar content ({}) getOperation", sweets.getSugarContent());
+                LOGGER.info("Sweet with sugar content ({}) retrieved value {}", sweets.getSugarContent(), sweets);
             }
+        }
+        if (foundSweets.size() == 0) {
+            LOGGER.warn("Sweet with sugar content ({} - {}) not found", startPosition, finalPosition);
         }
         return foundSweets;
     }
